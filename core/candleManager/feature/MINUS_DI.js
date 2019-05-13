db96be4454547bc7641586d5f1f762573fb70602
Dong-Nhan <ddnn97@hotmail.com>
2019-05-13 10:19:10 +0700

    add indicators: PLUS_DI, MINUS_DI

---
+++ core/candleManager/feature/MINUS_DI.js
@@ -0,0 +1,45 @@
+const talib = require('talib');
+const _ = require('lodash');
+
+class MinusDirectionalIndicator {
+  constructor({
+    period
+  }) {
+    this.period = period || 14;
+    this.executedResults = [];
+  }
+
+  update(index, candles) {
+    return new Promise((resolve, reject) => {
+      //calculate once for all
+      if (index === 0) {
+        talib.execute({
+          name: "MINUS_DI",
+          startIdx: 0,
+          endIdx: candles.length - 1,
+          high: _.map(candles, 'high'),
+          low: _.map(candles, 'low'),
+          close: _.map(candles, 'close'),
+          optInTimePeriod: this.period
+        }, (err, result) => {
+          if (err) {
+            console.log(err);
+            reject(-1);
+          }
+          const rawResult = result.result.outReal;
+          // fill 'ghost' results for those optInPeriod candles
+          this.executedResults = Array(candles.length - rawResult.length).fill(-1).concat(rawResult)
+          resolve(this.executedResults[0]);
+        })
+      } else {
+        resolve(this.executedResults[index]);
+      }
+    })
+  }
+
+  historyCandlesRequire() {
+    return this.period;
+  }
+}
+
+module.exports = MinusDirectionalIndicator
