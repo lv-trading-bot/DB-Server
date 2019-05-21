cd8f486f416075a1fce3bf53d93549595744cefc
dongnhan <ddnn97@hotmail.com>
2019-05-21 14:06:47 +0700

    Revert "Feature/indicators"

--- routes/candles.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const candles = require('../controllers/candles');
-
-/* GET users listing. */
-router.post('/', candles.postCandles);
-
-module.exports = router;
